ab3c8388edd0532dffebdd1558617f6e5474f998
Dilkash <dilkashpeshimam@gmail.com>
2022-11-16 13:09:07 +0530

    created categories component of store left

--- src/components/Store/StoreLeft/Categories/Categories.js
+++ src/components/Store/StoreLeft/Categories/Categories.js
@@ -2,7 +2,16 @@ import React from 'react'
 import './Categories.css'
 const Categories = () => {
     return (
-        <div>Categories</div>
+        <div className='categories'>
+            <h3 className='categories__title'>Categories</h3>
+            <ul className='product__categories'>
+                <li className='cat__item'>Album</li>
+                <li className='cat__item'>T-shirts</li>
+                <li className='cat__item'>Accessories</li>
+
+            </ul>
+
+        </div>
     )
 }
 
