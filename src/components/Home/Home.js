f2b8a76df9da904b12eefa3043092737dc51ad5f
Dilkash <dilkashpeshimam@gmail.com>
2022-11-20 13:46:30 +0530

    created home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import './Home.css'
+import HoodiesPng from '../assets/hoodie.png'
+import ProductItems from '../Store/StoreRight/ProductItems/ProductItems'
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home'>
+            <div className='home__container'>
+                <div className='home__container1' >
+                    <h2>
+                        NEW HOODIES COLLECTION
+                    </h2>
+                    <button className='home__btn'>BUY NOW</button>
+                </div>
+                <div className='home__container2'>
+                    <img style={{ width: '400px', height: '500px' }} src={HoodiesPng} />
+                </div>
+
+            </div>
+            <ProductItems />
+        </div>
     )
 }
 
