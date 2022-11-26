6abf43ea72a1ea6808b68f0a431fc01d4efca71b
Dilkash <dilkashpeshimam@gmail.com>
2022-11-26 17:12:29 +0530

    added product quantity in product detail page

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -6,6 +6,7 @@ import CartContext from '../../store/cartContext'
 import ReactImageMagnify from 'react-image-magnify';
 import { height, width } from '@mui/system'
 import ProductDetailReview from './ProductDetailReview'
+import ProductQuantity from '../ProductQuantity/ProductQuantity'
 
 
 const ProductDetail = () => {
@@ -17,6 +18,27 @@ const ProductDetail = () => {
     const { addProduct, } = useContext(CartContext)
     const refs = useRef([])
     refs.current = []
+
+    const handleAdd = () => {
+        if (quantity <= 4) {
+            setQuantity((prev) => prev + 1)
+
+
+
+        }
+
+
+    }
+
+
+    const handleSubtract = () => {
+        if (quantity <= 1) {
+            setQuantity(1)
+        } else {
+            setQuantity(quantity - 1)
+
+        }
+    }
     useEffect(() => {
         let data = Data
         let product = data.filter((item) => {
@@ -81,23 +103,24 @@ const ProductDetail = () => {
                             <h2 style={{ color: '#4e4e4e' }}>{productDetail.title}</h2>
 
                         </div>
-                        <div style={{ paddingTop: '1px' }} className='productDetail__price'>
+                        <div style={{ fontSize: '20px' }} className='productDetail__price'>
                             <h4 style={{ color: '#4e4e4e', fontWeight: 'bolder' }}>Rs {productDetail.price}</h4>
 
                         </div>
-                        <div className='productDetail__sizesContainer'>
-                            <h4>Sizes</h4>
-                            <div>
-                                <button className='product__size'>S</button>
-                                <button className='product__size'>M</button>
-                                <button className='product__size'>L</button>
-                                <button className='product__size'>XL</button>
-                                <button className='product__size'>XLL</button>
+                        <div className='productDetail__quantityContainer' >
+                            <div style={{ display: 'flex', marginBottom: '25px', marginTop: '30px' }} className='productDetail_quantity'>
+                                <div onClick={handleSubtract} className='quantity__subtract'>
+                                    <span style={{ marginLeft: '12px' }}>-</span>
+                                </div>
+                                <div className='quantity'>
+                                    {quantity}
+                                </div>
 
+                                <div onClick={handleAdd} className='quantity__add'>
+                                    <span style={{ marginLeft: '9px' }}> +</span>
+                                </div>
                             </div>
-
                         </div>
-
                         <div className='productDetail__buttonContainer'>
                             <button onClick={() => {
                                 addProduct({
@@ -105,8 +128,9 @@ const ProductDetail = () => {
                                     title: productDetail.title,
                                     price: productDetail.price,
                                     img: productDetail.imageUrl,
-                                    quantity: productDetail.quantity
+                                    quantity: quantity
                                 })
+                                setQuantity(1)
                             }} className='productDetail__button'
                             > ADD TO CART</button>
                         </div>
