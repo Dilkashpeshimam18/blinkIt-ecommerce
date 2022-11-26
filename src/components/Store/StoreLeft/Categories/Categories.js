1201f50ed0af2d6820efdf6726e420da7f8694ea
Dilkash <dilkashpeshimam@gmail.com>
2022-11-26 19:36:26 +0530

    added filter by category functionality in store page

--- src/components/Store/StoreLeft/Categories/Categories.js
+++ src/components/Store/StoreLeft/Categories/Categories.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CartContext from '../../../../store/cartContext'
 import './Categories.css'
+import { Data } from '../../../Data/Data'
+
 const Categories = () => {
+    const { data, setData } = useContext(CartContext)
+
+    const handleAlbum = () => {
+        let data = Data
+        setData(data)
+        const filter = data.filter((product) => {
+            return product.category == 'Album'
+        })
+        setData(filter)
+    }
+    const handleAccessories = () => {
+        let data = Data
+        setData(data)
+
+        const filter = data.filter((product) => {
+
+            return product.category == 'Accessories'
+        })
+        setData(filter)
+    }
+    const handleTshirt = () => {
+        let data = Data
+        setData(data)
+
+        const filter = data.filter((product) => {
+            return product.category == 'T-Shirt'
+        })
+        setData(filter)
+    }
+
     return (
         <div className='categories'>
             <h3 className='categories__title'>Categories</h3>
             <ul className='product__categories'>
-                <li className='cat__item'>Album</li>
-                <li className='cat__item'>T-shirts</li>
-                <li className='cat__item'>Accessories</li>
+                <li onClick={handleAlbum} className='cat__item'>Album</li>
+                <li onClick={handleTshirt} className='cat__item'>T-shirts</li>
+                <li onClick={handleAccessories} className='cat__item'>Accessories</li>
 
             </ul>
 
