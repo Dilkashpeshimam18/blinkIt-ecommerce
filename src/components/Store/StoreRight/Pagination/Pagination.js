c0a751b92ce3161d46e2b51870dd995aab89328c
Dilkash <dilkashpeshimam@gmail.com>
2022-11-29 12:10:55 +0530

    added styling to pagination

--- src/components/Store/StoreRight/Pagination/Pagination.js
+++ src/components/Store/StoreRight/Pagination/Pagination.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import './Pagination.css'
+import NextArrow from '../../../assets/next.png'
+
 
 const Pagination = ({ productPerPage, totalProduct, paginate }) => {
     const pageNumbers = []
@@ -6,14 +9,16 @@ const Pagination = ({ productPerPage, totalProduct, paginate }) => {
     for (let i = 1; i <= Math.ceil(totalProduct / productPerPage); i++) {
         pageNumbers.push(i)
     }
-
+    let lastIndex = pageNumbers[pageNumbers.length - 1]
     return (
-        <div>
-            <ul>
+        <div className='pagination'>
+            <ul style={{ display: 'flex' }}>
                 {pageNumbers.map((number) => (
-                    <li style={{ cursor: 'pointer' }} onClick={() => paginate(number)} key={number}><a href='!#'></a>{number}</li>
+                    <li className='pagination__Number' onClick={() => paginate(number)} key={number}><a href='!#'></a>{number}</li>
                 ))}
             </ul>
+
+
         </div>
     )
 }
