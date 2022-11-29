f7d16095a9b8e5c236b290f7804273c0be960710
Dilkash <dilkashpeshimam@gmail.com>
2022-11-29 12:41:18 +0530

    added routes in sales item

--- src/components/Store/StoreLeft/Sales/Sales.js
+++ src/components/Store/StoreLeft/Sales/Sales.js
@@ -1,33 +1,24 @@
 import React from 'react'
 import './Sales.css'
+import { SalesData } from '../../../Data/SalesData'
+import { Link } from 'react-router-dom'
 const Sales = () => {
     return (
         <div className='sales'>
             <h3>Sales</h3>
-            <div style={{ cursor: 'pointer' }} className='sales__container'>
-                <img className='sales__img' src='https://cdn.shopify.com/s/files/1/0022/5011/3123/products/shutdowndadhat.png?v=1663265819' />
-                <div className='sales__body'>
-                    <span className='sales__title'>SHUT DOWN HAT</span>
-                    <span className='sales__price'>Rs 300</span>
-                </div>
+            {SalesData.map((item) => {
+                return (
+                    <div style={{ cursor: 'pointer' }} className='sales__container'>
+                        <img className='sales__img' src={item.imageUrl} />
+                        <div className='sales__body'>
+                            <Link style={{ textDecoration: 'none' }} to={'/product-detail/' + item.productId}>  <span className='sales__title'>{item.title}</span></Link>
+                            <span className='sales__price'>Rs <del>{item.oldPrice}</del> {item.price}</span>
+                        </div>
+                    </div>
 
-            </div>
-            <div style={{ cursor: 'pointer' }} className='sales__container'>
-                <img className='sales__img' src='https://cdn.shopify.com/s/files/1/0022/5011/3123/products/BPSG_DyeF_faec3f21-5d3a-46e7-8beb-c863ba115477.png?v=1598016743' />
-                <div className='sales__body'>
-                    <span className='sales__title'>ICE CREAM CREWNECK PULLOVER</span>
-                    <span className='sales__price'>Rs 600</span>
-                </div>
+                )
+            })}
 
-            </div>
-            <div style={{ cursor: 'pointer' }} className='sales__container'>
-                <img className='sales__img' src='https://cdn.shopify.com/s/files/1/0022/5011/3123/products/SCRUNCHIE_587x.png?v=1611673302' />
-                <div className='sales__body'>
-                    <span className='sales__title'>THE SHOW SCRUNCHIE SET</span>
-                    <span className='sales__price'>Rs 200</span>
-                </div>
-
-            </div>
         </div>
     )
 }
