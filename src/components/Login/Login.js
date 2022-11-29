407520bb51af5a77509ee4d7d4dd1f31237fad6e
Dilkash <dilkashpeshimam@gmail.com>
2022-11-29 20:34:03 +0530

    added styling to login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import './Login.css'
 import axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom'
 import CartContext from '../../store/cartContext'
+import SubSection from '../SubSection/SubSection'
 
 const Login = () => {
     const [email, setEmail] = useState('')
@@ -40,20 +41,30 @@ const Login = () => {
 
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <h3>Email</h3>
-                    <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div>
-                    <h3>Password</h3>
-                    <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <button>Login</button>
-            </form>
-            <Link to='/sign-up'> <p style={{ cursor: 'pointer' }}>Create new accout?</p></Link>
+        <div className='login'>
+            <SubSection section='Login' />
+            <div className='login__container'>
+                <div className='login__subContainer'>
+                    <form onSubmit={handleSubmit}>
 
+                        <h2>Login</h2>
+                        <div className='login__inputContainer'>
+                            <h3 className='login__subTitle'>Email</h3>
+                            <input className='login__input' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className='login__inputContainer'>
+                            <h3 className='login__subTitle'>Password</h3>
+                            <input className='login__input' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <button className='login__button'>LOG IN</button>
+                    </form>
+                    <div className='login__link'>
+                        <Link style={{ textDecoration: 'none', color: 'black', fontWeight: 'bolder' }} to='/sign-up'> <h3 style={{ cursor: 'pointer', fontSize: '13px', textDecoration: 'none' }}>Create new account?</h3></Link>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
