6de8de00c7746fba0d222c134e2ccd3e3889a48e
Dilkash <dilkashpeshimam@gmail.com>
2023-01-08 18:30:10 +0530

    about page responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,12 +12,12 @@ const About = () => {
 
             <div className='about__container'>
                 <div className='about__imgContainer'>
-                    <Avatar style={{ width: '300px', height: '300px' }} src='https://render.fineartamerica.com/images/rendered/square-product/small/images/rendered/default/flat/round-beach-towel/images/artworkimages/medium/3/5-blackpink-lala-lazodi-transparent.png?&targetx=139&targety=120&imagewidth=510&imageheight=548&modelwidth=788&modelheight=788&backgroundcolor=000000&orientation=0' />
+                    <Avatar className='about__img' src='https://render.fineartamerica.com/images/rendered/square-product/small/images/rendered/default/flat/round-beach-towel/images/artworkimages/medium/3/5-blackpink-lala-lazodi-transparent.png?&targetx=139&targety=120&imagewidth=510&imageheight=548&modelwidth=788&modelheight=788&backgroundcolor=000000&orientation=0' />
 
                 </div>
                 <div className='about__contentContainer'>
-                    <h2 style={{ fontWeight: 'bolder', fontStyle: 'italic', }}>BLINKS IT</h2>
-                    <p style={{ fontWeight: 'bold', fontStyle: 'italic' }}>
+                    <h2 className='about__title' style={{ fontWeight: 'bolder', fontStyle: 'italic', }}>BLINKS IT</h2>
+                    <p className='about__content' style={{ fontWeight: 'bold', fontStyle: 'italic' }}>
                         BLINK IT is a onlline shopping store where you can buy blackpink latest merch & album at reasonable rate in India.
                         Choose latest T-shirts & accessories from wide choice of collection. Secure payment & shipping 4-5 business days. Made in India with love.
                     </p>
