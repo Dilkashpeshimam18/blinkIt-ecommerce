28b36d526be04891a2b2841c577a0ac97ed25150
Dilkash <dilkashpeshimam@gmail.com>
2023-01-30 12:39:23 +0530

    changes in config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCAfj9rxGTKyrdIeCMo7Yxs3vATRueaX3g",
-    authDomain: "crud-d96e0.firebaseapp.com",
-    projectId: "crud-d96e0",
-    storageBucket: "crud-d96e0.appspot.com",
-    messagingSenderId: "355796991849",
-    appId: "1:355796991849:web:b15c0140e9f79250e087f8",
-    measurementId: "G-QFDVC2DWB7"
+    apiKey: "AIzaSyC3f1iaa9UJOVVIn8rnVJlImKeyS3xXSkM",
+    authDomain: "blinkit-ecommerce-dcfe7.firebaseapp.com",
+    projectId: "blinkit-ecommerce-dcfe7",
+    storageBucket: "blinkit-ecommerce-dcfe7.appspot.com",
+    messagingSenderId: "237122808150",
+    appId: "1:237122808150:web:56693ccf4a712539050ef7",
+    measurementId: "G-Q3HDN14V1W"
 }
 
 const app = initializeApp(firebaseConfig)
